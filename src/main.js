acc6740f18c80d44bcca98d2a255ed295da04095
pbehera-techrayslabs <pbehera@techrayslabs.com>
2026-02-07 19:04:33 +0530

    added tasklist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
-let a = "654vuvug";
-console.log(a);
+let tasks = [];
+const button = document.getElementById("add-btn");
+const template = document.getElementById("task-template");
+const tasklist = document.getElementById("tasklist");
+button;
+addEventListener("click", addtask);
+function addtask() {
+    const titleinput = document.getElementById("title");
+    const dateinput = document.getElementById("date");
+    if (titleinput.value === "" || dateinput.value === "") {
+        alert("please fill out all fields");
+        return;
+    }
+    const task = {
+        title: titleinput.value,
+        date: dateinput.value,
+        status: "pending"
+    };
+    tasks.push(task);
+    displayTasks();
+    titleinput.value = "";
+    dateinput.value = "";
+}
+function displayTasks() {
+    tasklist.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        tasklist.innerHTML +=
+            `<li> 
+            ${task.title} - ${task.date} -${task.status}
+            </li>`;
+    });
+}
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,50 @@
-let a : string ="654vuvug";
-console.log(a);
+interface Task {
+    title: string;
+    date: string;
+    status: string;
+}
+
+let tasks: Task[] = [];
+
+
+
+    const button = document.getElementById("add-btn") as HTMLButtonElement;
+   
+    const template = document.getElementById("task-template") as HTMLTemplateElement;
+       const tasklist = document.getElementById("tasklist") as HTMLDivElement;
+       button addEventListener("click",addtask);
+
+
+function addtask() {
+     const titleinput = document.getElementById("title") as HTMLInputElement;
+    const dateinput = document.getElementById("date") as HTMLInputElement;
+  if(titleinput.value ==="" || dateinput.value ===""){
+    alert("please fill out all fields");
+    return;
+  }
+   
+    const task: Task = {
+        title: titleinput.value,
+        date: dateinput.value,
+        status: "pending"
+    };
+    tasks.push(task);
+    displayTasks();
+    
+
+    titleinput.value = "";
+    dateinput.value = "";
+}
+
+function displayTasks() {
+  
+    tasklist.innerHTML ="";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+            tasklist.innerHTML +=
+            `<li> 
+            ${task.title} - ${task.date} -${task.status}
+            </li>`;
+    });
+}
